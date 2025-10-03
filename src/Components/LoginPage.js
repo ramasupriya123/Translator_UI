c0937a8e8a9195b58613cd49d04d8aeec43c5b3c
ramasupriya123 <ramasupriyavankayalapati@gmail.com>
2025-10-03 16:20:07 +0530

    Update LoginPage.js

--- src/Components/LoginPage.js
+++ src/Components/LoginPage.js
@@ -47,7 +47,8 @@ const LoginPage = () => {
         email: email,
         password: password,
       })
-      const response = await axios.post(`http://localhost:5038/api/Register/login`, {
+      const response = await axios.postpost(
+  `https://miracle-translator-api-g4gxfbeqgmg7e7g8.eastus-01.azurewebsites.net/api/Register/login`, {
         email: email,
         password: password,
       });
